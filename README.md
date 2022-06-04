00bc3a7e2ad01fd3a481d99c0c3b15abd5537828
Alex Quispe <wilmer.quispe@agetic.gob.bo>
2022-06-04 14:35:01 -0400

    feat: validate version :sparkles:

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # node-check
+
+Verifica que la versión de node sea la correcta cuando se levanta una aplicación de node.
+
+Puede verificar las siguientes aplicaciones: `node`, `npm` y `yarn` las cuales deben ser especificadas en el archivo `package.json`.
+
+## Instalación
+
+```bash
+npm install -g node-check
+```
+
+## Ejemplo:
+
+Si en el archivo `package.json` tenemos la siguiente configuración:
+
+```json
+{
+  "name": "my-project",
+  "version": "1.0.0",
+  ...
+  "engines": {
+    "node": "^16",
+    "npm": "^8"
+  }
+}
+```
+
+Luego desde la raiz del proyecto ejecutamos el siguiente comando:
+
+```bash
+node-check
+```
+
+Resultado si tenemos activada la versión correcta de node:
+
+```bash
+node: 16.13.2
+npm: 8.12.1
+```
+
+en caso contrario:
+
+```bash
+node: 12.22.7 (versión requerida: ^16)
+npm: 6.14.17 (versión requerida: ^8)
+```
---
+++ src/main.ts
@@ -0,0 +1,109 @@
+import { exec, ExecException } from "child_process";
+import compareVersions from "compare-versions";
+import path from "path";
+
+const APP_LIST = ["node", "npm", "yarn"];
+const PROJECT_PATH = process.cwd();
+
+const cmd = (command: string, executePath: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    exec(
+      command,
+      { cwd: executePath },
+      (error: ExecException | null, stdout: string, stderr: string) => {
+        if (error) reject(stderr);
+        if (!error) resolve(stdout);
+      }
+    );
+  });
+};
+
+async function getPackageJson() {
+  try {
+    const packageJsonPath = path.resolve(PROJECT_PATH, "package.json");
+    const packageJson = await import(packageJsonPath);
+    return packageJson;
+  } catch (e) {
+    return {};
+  }
+}
+
+async function versionValid(appName: string): Promise<boolean> {
+  const currentVersion = (await cmd(`${appName} --version`, PROJECT_PATH))
+    .trim()
+    .replace("v", "");
+
+  const packageJson = await getPackageJson();
+  const requiredVersion =
+    packageJson.engines?.[appName] || packageJson.volta?.[appName];
+
+  const isValid =
+    !requiredVersion ||
+    compareVersions.satisfies(currentVersion, requiredVersion);
+
+  const result = isValid
+    ? `\x1b[92m${appName}: ${currentVersion}\x1b[0m\n`
+    : `\x1b[91m${appName}: ${currentVersion}\x1b[0m \x1b[93m(versión requerida: ${requiredVersion})\x1b[0m\n`;
+
+  process.stdout.write(result);
+  return isValid;
+}
+
+async function getApps(): Promise<string[]> {
+  const packageJson = await getPackageJson();
+  const appSet = new Set<string>();
+
+  if (packageJson.engines)
+    Object.keys(packageJson.engines).map((key) => appSet.add(key));
+
+  if (packageJson.volta)
+    Object.keys(packageJson.volta).map((key) => appSet.add(key));
+
+  const appSelectedList = Array.from(appSet);
+  return APP_LIST.filter((app) => appSelectedList.includes(app));
+}
+
+async function init() {
+  // SHOW APP VERSION
+  if (
+    process.argv.join(" ").includes(" --version") ||
+    process.argv.join(" ").includes(" -v")
+  ) {
+    const version = (await import(`${__dirname}/../package.json`)).version;
+    return process.stdout.write(`${version}\n`);
+  }
+
+  // EXECUTE APP
+  const appList = await getApps();
+
+  if (appList.length === 0) {
+    const msg = `\x1b[92mNada que validar.\x1b[0m
+
+Puede especificar la versión de node desde el archivo package.json
+
+Ejemplo:
+
+{
+  "name": "project",
+  ...
+  "engines": {
+    "node": "^16",
+    "npm": "^8"
+  }
+}\n`;
+    return process.stdout.write(msg);
+  }
+
+  let isValid = true;
+
+  for (const appItem of appList) {
+    if (!(await versionValid(appItem))) isValid = false;
+  }
+
+  if (!isValid) process.exit(1);
+}
+
+init().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
